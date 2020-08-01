497fefd52ea3fe5b4d66c4e2b14feecb550b7b40
WachaDev <josemrr27@gmail.com>
2020-08-01 18:25:55 -0400

    lo mismo del anterior

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,20 @@ public class PlayerController : MonoBehaviour
     [Range(0f, 20f)] public float speed = 10f;
 
     [Header("Jump")]
-    public float gravity = -9.81f;
+    private float _gravity = -9.81f;
     [Range(0, 100)]
-    public float mass = 1;
-    public Vector3 velocity;
-    public float jumpForce = 0.5f;
+    [SerializeField] private float _mass = 1;
+    private Vector3 _velocity;
+    [SerializeField] private float jumpForce = 0.5f;
     public Transform groundChecker;
-    public float radius = 0.5f;
+    private float _radius = 0.5f;
     public LayerMask ground;
     bool isGrounded;
 
     [Header("Camera ajusment")]
     public Transform cam;
-    public float turnSmoothTime = 0.1f;
-    private float turnSmoothVelocity;
+    public float _turnSmoothTime = 0.1f;
+    private float _turnSmoothVelocity;
     public float sensivity = 0.1f;
 
     void Start()
@@ -47,7 +47,7 @@ public class PlayerController : MonoBehaviour
         if (movement.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 movDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
@@ -59,17 +59,17 @@ public class PlayerController : MonoBehaviour
 
     void PlayerJump()
     {
-        isGrounded = Physics.CheckSphere(groundChecker.position, radius, ground);
+        isGrounded = Physics.CheckSphere(groundChecker.position, _radius, ground);
 
-        velocity.y += (gravity * mass * Time.deltaTime);
+        _velocity.y += (_gravity * _mass * Time.deltaTime);
 
-        characterController.Move(velocity * Time.deltaTime);
+        characterController.Move(_velocity * Time.deltaTime);
 
-        if (isGrounded && velocity.y < 0)
-            velocity.y = -2f;
+        if (isGrounded && _velocity.y < 0)
+            _velocity.y = -2f;
         
 
         if (Input.GetKeyDown("space") && isGrounded)
-            velocity.y = Mathf.Sqrt(jumpForce * velocity.y * gravity);
+            _velocity.y = Mathf.Sqrt(jumpForce * _velocity.y * _gravity);
     }
 }
